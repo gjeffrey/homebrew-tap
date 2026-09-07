81b8ef69dbccd3c58a098c0b3200a3eda3e7e048
Gabriel Jeffrey <gabrieljeffrey@gmail.com>
2026-09-06 20:30:52 -0700

    earshot 0.9.4

--- Casks/earshot.rb
+++ Casks/earshot.rb
@@ -1,6 +1,6 @@
 cask "earshot" do
-  version "0.9.3,202609061509"
-  sha256 "fc76d300b0b4ded64ac709720a8e7c40789f6af7759bd6a55b9ca42ffb6a2c89"
+  version "0.9.4,202609062029"
+  sha256 "3e73ab7b8dd532533cef18eeb1e79fe3b485973b55b3674dd92ec7857cd9f124"
 
   url "https://www.gabrieljeffrey.com/earshot/Earshot.zip"
   name "Earshot"
