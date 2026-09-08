5e722fef6e0bf743d9195b4dcd04bdf8e504ab56
Gabriel Jeffrey <gabrieljeffrey@gmail.com>
2026-09-08 14:45:22 -0700

    lightbox 1.0 (202609081444)

--- Casks/lightbox.rb
+++ Casks/lightbox.rb
@@ -1,8 +1,8 @@
 cask "lightbox" do
-  version "1.0,202609071819"
-  sha256 "a9a0edd538c1fb899d0974d59f41487f0dd13ab9ae5970edcd7d9bfc1d4b8f26"
+  version "1.0,202609081444"
+  sha256 "d23cfe2535047e0899c4e7f24aa2ffe6a2f27659649d9cc9e0f07d96d25fdf62"
 
-  url "https://www.gabrieljeffrey.com/lightbox/releases/Lightbox-1.0-202609071819.zip"
+  url "https://www.gabrieljeffrey.com/lightbox/releases/Lightbox-1.0-202609081444.zip"
   name "Lightbox"
   desc "Media browser for photographers — open a folder, see everything"
   homepage "https://gabrieljeffrey.com/lightbox/"
